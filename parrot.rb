0848e4341e6bcda3344f5e927575e44d558a8b00
Amari Ellington <amari.l.ellington@gmail.com>
2019-02-11 19:39:30 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def parrot(i = "Squawk!")
   puts i
   return "Squawk!"
@@ -6,5 +7,13 @@ end
 parrot "Squawk!"
 parrot
 parrot "Pretty bird!"
+=======
+def parrot("squawk!")
+   "Pretty bird!"
+
+end
+
+puts "That is such a #{"squawk!"}"
+>>>>>>> b268f5cd44e11f2e33d30d37a4e37d140dfbae0c
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
